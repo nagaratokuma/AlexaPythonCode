4e145eed159447eef39b2df912457ce651aa2986
nagaratokuma <nagaratokuma@gmail.com>
2022-11-22 04:42:15 +0900

    telegram_bot test

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,7 +1,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
  
 # アクセストークン（先ほど発行されたアクセストークンに書き換えてください）
-TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+TOKEN = "test"
  
  
 class TelegramBot:
